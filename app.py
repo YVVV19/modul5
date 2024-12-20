208fb12da3a52a98b3be5a4cb4fc88d80a65ac2f
YVVV19 <vadimyankovskiy2000@gmail.com>
2024-12-20 19:16:05 +0200

    ...

---
+++ app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Path, Header, Query
+from datetime import datetime
+
+app=FastAPI()
+
+@app.get("/data/")
+async def get_data(
+    user_id: int = Path(...),
+    timestamp: str = Query(default=None),
+    client: str = Header(...),
+    ):
+    if not timestamp:
+        timestamp == datetime.timestamp()
+        return{"Hello":client,
+            "user_id:":user_id,
+            "time:":timestamp,}
+    return{"Hello":client,
+            "user_id:":user_id,
+            "time:":timestamp,}
